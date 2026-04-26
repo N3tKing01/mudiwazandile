396e4ff284ad3ecd06e1560261538c9773a2b143
TelKing01 <joshuamambozindoga03@gmail.com>
2026-04-26 15:33:28 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,28 @@
+
+/**
+* supreme
+* MidKing01
+* JoshKing 
+* MidKBAN
+* Silent_LDT
+*/
+
+const settings = {
+  packname: '✮⃝MidKing',
+  author: '☠️NetKing💝',
+  botName: "CRAZY_X",
+  botOwner: '✮⃝MidKing', // Your name
+  ownerNumber: process.env.OWNER_NUMBER || '263716729222', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.6.6",
+  updateBranch: 'main',
+  updateZipToken: '',
+  updateZipUrl: "https://github.com/N3tKing01/mudiwazandile/archive/refs/heads/main.zip",
+  timezone: 'Africa/Harare',
+};
+
+module.exports = settings;
